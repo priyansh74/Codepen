96e1335241d60a12848661b07dbfffb4e65ccff0
priyansh74 <priyanshjha99@gmail.com>
2024-05-12 11:47:22 +0530

    code workspace v1.0

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import Editor from "./Editor";
 import { useState, useEffect } from "react";
 
 function App() {
+  
   const [html, setHtml] = useState("<h1>Start Coding here..</h1>");
   const [css, setCss] = useState("");
   const [javascript, setJavascript] = useState("");
@@ -19,14 +20,15 @@ function App() {
     "</script>";
 
   const srcDoc = "<html>" + srcDoc1 + "</html>";
+   
 
   useEffect(() => {
     setTimeout(() => setPreview(srcDoc), 2000);
-  });
+  },[srcDoc]);
 
   return (
     <>
-      <div className="top-card">
+      <div  className="top-card">
         <div className="nav">
           <table>
             <th colSpan="1">
@@ -55,16 +57,27 @@ function App() {
             </th>
           </table>
         </div>
-        <div className="App">
+        
+
+        <div  className="App">
+          
+          <div className="editor1">
+          <Editor 
+          
+          answer="HTML"
+          imageV="https://img.freepik.com/free-icon/html-5_318-674234.jpg"
+          mode="html"
+          onChange={(e) => {
+            setHtml(e);
+          }}
+          value={html}
+        />
+          </div>
+          <div  className="editor2">
           <Editor
-            className="Editors"
-            mode="html"
-            onChange={(e) => {
-              setHtml(e);
-            }}
-            value={html}
-          />
-          <Editor
+            
+            answer="CSS"
+            imageV="https://cdn.worldvectorlogo.com/logos/css-3.svg"
             mode="css"
             onChange={(e) => {
               setCss(e);
@@ -72,15 +85,25 @@ function App() {
             value={css}
           />
 
+          </div>
+          <div  className="editor3">
+           
           <Editor
-            mode="javascript"
-            onChange={(e) => {
-              setJavascript(e);
-            }}
-            value={javascript}
-          />
+           
+           answer="JavaScript"
+           imageV="https://cdn.iconscout.com/icon/free/png-256/free-javascript-2038874-1720087.png"
+           mode="javascript"
+           onChange={(e) => {
+             setJavascript(e);
+           }}
+           value={javascript}
+         />
+          </div>
+          
         </div>
+
       </div>
+
       <div>
         <div>
           <iframe
@@ -91,6 +114,9 @@ function App() {
             width="100%"
             height="100%"
           />
+        </div>
+        <div>
+      
         </div>
       </div>
       <p className="footer">Developed By : Priyansh Jha</p>
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,29 +1,59 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/mode-css";
 import "ace-builds/src-noconflict/mode-html";
 import "ace-builds/src-noconflict/theme-twilight";
-import "ace-builds/src-noconflict/ext-language_tools"
-
+import "ace-builds/src-noconflict/ext-language_tools";
 
 const Editor = (data) => {
+  
+  
+  const [isShown, setIsShown] = useState(true);
+  
+  const handleClick = event => {
+    //element visibilty
+    setIsShown(current => !current);
+  };
+ 
   return (
     <div>
-        <AceEditor
-        className='AceEditor'
+      <div className="editor-head">
+        <p className="editorName">
+          {data.answer}{" "}
+          <img
+            onClick={handleClick}
+            className="editorImg"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS02dXAAvqLmHDKHP1ZqjIbEACFOXlHSlVlnw&usqp=CAU"
+            alt="ClickDown"
+          />{" "}
+        </p>
+      </div>
+      
+      {isShown && <AceEditor
+       onLoad={editorInstance => {
+        editorInstance.container.style.resize = "horizontal";
+        document.addEventListener("mouseup", e => (
+          editorInstance.resize()
+        ));
+      }}
+        className="AceEditor"
         mode={data.mode}
-        width="480px"
+        width="435px"
         height="290px"
         theme="twilight"
+       
         onChange={data.onChange}
         fontSize={16}
         showPrintMargin={true}
         showGutter={true}
         highlightActiveLine={true}
+        
         value={data.value}
         wrapEnabled={true}
         setOptions={{
+          
           useWorker: false,
           enableBasicAutocompletion: true,
           enableLiveAutocompletion: true,
@@ -31,10 +61,9 @@ const Editor = (data) => {
           showLineNumbers: true,
           tabSize: 2,
         }}
-        />
-
+      />}
     </div>
-  )
-}
+  );
+};
 
 export default Editor;
